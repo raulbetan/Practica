904944832a6eb2970269fd8d7675dd8777ae5db3
raulbetan <raulbetancourt25@gmail.com>
2020-05-20 20:58:13 -0500

    Revert "Subiendo el proyecto Ejemplos_Cap11 a github"
    This reverts commit 0c9265bbf4f36509d3824c213107c375803efd3c.

---
+++ src/ejem_11_19/DemoAreaTexto.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejem_11_19;
+
+/**
+ *
+ * @author BETANCOURT
+ */
+// Copia el texto seleccionado de un área de texto a otra.
+import javax.swing.JFrame;
+
+public class DemoAreaTexto
+{
+public static void main( String args[] )
+{
+MarcoAreaTexto marcoAreaTexto = new MarcoAreaTexto();
+marcoAreaTexto.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+marcoAreaTexto.setSize( 425, 200 ); // establece el tamaño del marco
+marcoAreaTexto.setVisible( true ); // muestra el marco
+} // fin de main
+} // fin de la clase DemoAreaTexto
